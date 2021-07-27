b8997c0768343dbb28e566e257c558620bf9458c
Varun Ganta <gantavarun202@gmail.com>
2021-07-28 04:17:19 +0530

    Update GreatGraphs.cpp

--- GreatGraphs.cpp
+++ GreatGraphs.cpp
@@ -5,12 +5,6 @@ using namespace std;
 
 #define ll long long
 
-/*ll gcd(ll a, ll b) {
-   if(b==0)
-   return a;
-   return gcd(b, a%b);
-}*/
-
 int main()
 {
     ios_base::sync_with_stdio(false);
