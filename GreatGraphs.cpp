3672d3fdb00ed7dda70d24e3200d4386178c00a0
Varun Ganta <gantavarun202@gmail.com>
2021-07-28 04:16:41 +0530

    Create GreatGraphs.cpp

---
+++ GreatGraphs.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+/*ll gcd(ll a, ll b) {
+   if(b==0)
+   return a;
+   return gcd(b, a%b);
+}*/
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for(ll i=0; i<n; i++)
+            cin >> a[i];
+        sort(a.begin(), a.end());
+        ll s=0;
+        ll res=0;
+        for(ll i=0; i<n-2; i++)
+        {
+            s+=a[i];
+            res=res+s-a[i+2]*(i+1);
+        }
+        cout << res << "\n";
+    }
+
+    return 0;
+}
