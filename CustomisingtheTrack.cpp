96e03eb586a23f8920aaf6be6405d36ee823aad7
Varun Ganta <gantavarun202@gmail.com>
2021-07-28 04:18:34 +0530

    Create CustomisingtheTrack.cpp

---
+++ CustomisingtheTrack.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll s=0;
+        for(ll i=0; i<n; i++)
+        {
+            ll x;
+            cin >> x;
+            s+=x;
+        }
+        ll res=(s%n)*(n-(s%n));
+        cout << res << "\n";
+    }
+
+    return 0;
+}
